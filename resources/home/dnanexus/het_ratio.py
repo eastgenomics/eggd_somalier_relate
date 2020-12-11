e45a8dfb126c0ebad876873dac732c1118071e62
Aisha Dahir <AishaDahir75@gmail.com>
2020-12-11 15:18:57 +0000

    SCH-14 Add predicted sex from X het-homo ratio

--- resources/home/dnanexus/het_ratio.py
+++ resources/home/dnanexus/het_ratio.py
@@ -7,34 +7,35 @@ import pandas as pd
 #from matplotlib.colors import ListedColormap
 
 
+#### Read data in
 data = pd.read_csv('somalier.samples.tsv', sep='\t')
-dat = data.iloc[:,np.r_[1,18:25]]
-print(dat)
+dat = data.iloc[:,np.r_[1,18:25]] #select columns of interest
 
-het_ratio = dat.iloc[:,4]/dat.iloc[:,5]
-print(het_ratio)
-#dat.join(het_ratio)
-
+#### Create the Xhet_homo_ratio table and append to data frame
+het_ratio = dat.iloc[:,4]/dat.iloc[:,5] 
 het_ratio = pd.DataFrame({'X_het/X_homo_alt_ratio':het_ratio}) 
 data2 = pd.concat([data, het_ratio], axis=1)
 
-data2.to_csv('somalier.samples.tsv', sep='\t',index=False, header =True)
+#### Add Predicted Sex based on Xhet_homo_ratio
+PredictedSex = []
+het_ratio = data2['X_het/X_homo_alt_ratio']
+cutoff  = 0.6 #based on Peddy but can be changed 
 
-# colnames = dat2.columns
-# print(colnames)
+for x in het_ratio:
+    if x < cutoff:
+        PredictedSex.append("M")
+    else:
+        PredictedSex.append("F")
 
-# dat2 = dat.iloc[:,np.r_[1,4]]
-# print(dat2)
+Predicted_Sex = pd.DataFrame({'Predicted_Sex':PredictedSex}) 
 
-#color based on given information - create a random string of red and blue
-# stated_sex = ["male", "male", "female", "female"] + ["male"]*7
-# cols = []
-# for i in stated_sex:
-#     if i == "male":
-#         cols.append( "blue")
-#     else:
-#         cols.append("red")
 
+##### Append Predicted Sex to dataframe and save replace the file
+data3 = pd.concat([data2, Predicted_Sex], axis=1)
+print(data3)
+data3.to_csv('somalier.samples.tsv', sep='\t',index=False, header =True)
+
+##### Plotting
 
 # plt.scatter(x=data['X_het'],y=data['X_depth_mean'], color=cols)
 # plt.ylabel("X depth mean")
